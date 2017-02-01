7d3c2782bbda7c5f8a0221aaac0bea758e4c7a1f
hacker34 <johnny@hackinginnovatioins.com>
2017-01-31 18:39:33 -0700

    Setup Project
    Have project setup and a couple of files added.

---
+++ TacoPOP/TacoPOP/DropShadow.swift
@@ -0,0 +1,16 @@
+//
+//  DropShadow.swift
+//  TacoPOP
+//
+//  Created by Johnny Hacking on 1/31/17.
+//  Copyright © 2017 HackingInnovations. All rights reserved.
+//
+
+import UIKit
+
+protocol DropShadow{}
+
+extension DropShadow where Self: UIView{
+    
+    
+}
